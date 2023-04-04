6651e9695ecf1504bba8f74eb4c79c57786b0d5f
Jaden <ljaden.9@gmail.com>
2023-04-04 17:43:18 -0400

    testing different header on req

--- src/pages/api/standings/index.ts
+++ src/pages/api/standings/index.ts
@@ -2,70 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import type { StandingsType } from "@/utils/types";
 
-// import { getStandings } from "@/utils/api/api";
-import axios from "axios";
+import { getStandings } from "@/utils/api/api";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<StandingsType>
 ) {
-  // console.log(req.query);
-  // const data = await getStandings();
-  const url = `https://stats.nba.com/stats/leaguestandings?LeagueID=00&Season=2022-23&SeasonType=Regular+Season&SeasonYear=`;
-  const { data: standings } = await axios.get(url, {
-    headers: {
-      Host: "stats.nba.com",
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
-      Accept: "application/json, text/plain, */*",
-      "Accept-Language": "en-US,en;q=0.5",
-      "Accept-Encoding": "gzip, deflate, br",
-      "x-nba-stats-origin": "stats",
-      "x-nba-stats-token": "true",
-      Connection: "keep-alive",
-      Referer: "https://stats.nba.com/",
-      Pragma: "no-cache",
-      "Cache-Control": "no-cache",
-    },
-  });
-
-  const selectedHeaders = [
-    "TeamID",
-    "TeamCity",
-    "TeamName",
-    "TeamSlug",
-    "Conference",
-    "ConferenceRecord",
-    "PlayoffRank",
-    "Division",
-    "DivisionRank",
-    "WINS",
-    "LOSSES",
-    "WinPCT",
-    "HOME",
-    "ROAD",
-    "L10",
-    "Last10Home",
-    "Last10Road",
-    "CurrentStreak",
-    "strCurrentStreak",
-  ];
-  const { headers, rowSet } = standings.resultSets[0];
-
-  const result = rowSet.map((row: any) =>
-    row.reduce((obj: any, val: any, i: number) => {
-      if (selectedHeaders.includes(headers[i])) {
-        obj[headers[i]] = val;
-      }
-      return obj;
-    }, {})
-  );
-
-  const data = {
-    season: standings.parameters.SeasonYear,
-    standings: result,
-  };
-
-  console.log(data);
+  const data = await getStandings();
   res.status(200).json(data);
 }
--- src/utils/api/api.ts
+++ src/utils/api/api.ts
@@ -84,22 +84,11 @@ export async function getGameStats(gameId: string) {
 // Fetch standings
 export async function getStandings() {
   try {
-    // const url = `https://stats.nba.com/stats/leaguestandingsv3?LeagueID=00&Season=2022-23&SeasonType=Regular%20Season&Section=overall`;
-    const url = `https://stats.nba.com/stats/leaguestandings?LeagueID=00&Season=2022-23&SeasonType=Regular+Season&SeasonYear=`;
+    const url = `https://stats.nba.com/stats/leaguestandingsv3?LeagueID=00&Season=2022-23&SeasonType=Regular%20Season&Section=overall`;
+    // const url = `https://stats.nba.com/stats/leaguestandings?LeagueID=00&Season=2022-23&SeasonType=Regular+Season&SeasonYear=`;
     const { data: standings } = await axios.get(url, {
       headers: {
-        Host: "stats.nba.com",
-        "User-Agent":
-          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0",
-        Accept: "application/json, text/plain, */*",
-        "Accept-Language": "en-US,en;q=0.5",
-        "Accept-Encoding": "gzip, deflate, br",
-        "x-nba-stats-origin": "stats",
-        "x-nba-stats-token": "true",
-        Connection: "keep-alive",
-        Referer: "https://stats.nba.com/",
-        Pragma: "no-cache",
-        "Cache-Control": "no-cache",
+        Referer: "https://www.nba.com",
       },
     });
 
