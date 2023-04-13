00b0f4601bc2c466f2a092239c4672169da9fc64
Jaden <ljaden.9@gmail.com>
2023-04-13 00:31:16 -0400

    live score fetching in scoreboard component

--- src/components/Scoreboard/Scoreboard.tsx
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -1,4 +1,6 @@
-import type { ScheduleGameType } from "@/utils/types";
+import { useState } from "react";
+import Image from "next/image";
+import type { ScheduleGameType, GameType } from "@/utils/types";
 import { IoReturnDownBackOutline } from "react-icons/io5";
 
 // components
@@ -7,6 +9,9 @@ import Period from "./Period";
 import PlayerStats from "./PlayerStats";
 
 import moment from "moment";
+
+import { useQuery } from "@tanstack/react-query";
+import axiosFetcher from "@/utils/api/axiosFetcher";
 
 type Props = ScheduleGameType & {
   returnHome: () => void;
@@ -24,6 +29,16 @@ const Scoreboard = ({
   awayTeam,
   returnHome,
 }: Props) => {
+  const [isLive, setIsLive] = useState<boolean>(false);
+
+  const { data } = useQuery<GameType>({
+    queryKey: ["gameInfo", gameId],
+    queryFn: () => axiosFetcher(`/api/game/${gameId}`),
+    refetchInterval: (data) => (data?.gameStatus === 2 ? 10000 : false),
+    onSuccess: (data) => setIsLive(data.gameStatus === 2),
+    enabled: isLive,
+  });
+
   return (
     <section className="">
       <div className="bg-white shadow-2xl">
@@ -31,6 +46,7 @@ const Scoreboard = ({
           <span className="absolute left-1 bottom-3.5 cursor-pointer">
             <IoReturnDownBackOutline onClick={returnHome} size={18} />
           </span>
+
           {gameCode && gameStatus === 1 ? (
             <span className="uppercase text-lg font-semibold">
               {gameCodeToDT(gameCode)}
@@ -38,14 +54,40 @@ const Scoreboard = ({
             </span>
           ) : (
             <span className="uppercase text-lg font-semibold">
-              {gameStatusText}
+              {isLive ? (
+                <>
+                  {data?.gameStatusText}
+
+                  <span className="absolute left-25 bottom-3.5 px-2">
+                    <Image
+                      src="/live.svg"
+                      width={15}
+                      height={15}
+                      alt="live"
+                    ></Image>
+                  </span>
+                </>
+              ) : (
+                gameStatusText
+              )}
             </span>
           )}
         </div>
         <div className="grid grid-2 sm:grid-cols-2">
-          <Team {...awayTeam} isHome={false} gameStatus={gameStatus} />
-          <Team {...homeTeam} isHome={true} gameStatus={gameStatus} />
+          <Team
+            {...awayTeam}
+            isHome={false}
+            gameStatus={gameStatus}
+            liveScore={data?.awayTeam.score}
+          />
+          <Team
+            {...homeTeam}
+            isHome={true}
+            gameStatus={gameStatus}
+            liveScore={data?.homeTeam.score}
+          />
         </div>
+
         <div className="w-full text-center p-2 flex flex-col justify-center">
           <p>
             {arenaName}, {arenaCity}, {arenaState}
